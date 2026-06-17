bd9ab0deb6916c1dbbf2acfa345bd64c737eef4e
Pedro Roque <roquefiesp@gmail.com>
2026-06-17 10:07:41 -0300

    feat: implementa protecao JWT nas rotas de clientes e emprestimos e cria endpoint de login

--- src/routes.ts
+++ src/routes.ts
@@ -1,35 +1,42 @@
 import { Router } from "express";
 import type { Request, Response } from "express";
 
+// Importação do middleware de Autenticação
+import { Auth } from "./middleware/Auth.js"; // Ajuste o caminho da pasta se necessário
+
 const router = Router();
 
 import ClienteController from "./controller/ClienteController.js";
+import EmprestimoController from './controller/EmprestimoController.js';
 
 // ============================================
-// ROTA INICIAL
+// ROTA INICIAL & AUTENTICAÇÃO
 // ============================================
 router.get("/api", (req: Request, res: Response) => {
   res.status(200).json({ mensagem: "Olá, boas-vindas a API do OpenLine." });
 });
 
+// Rota onde o Front-end faz o POST enviando email e senha para receber o Token JWT
+router.post("/api/login", Auth.validacaoUsuario);
+
 
 // ============================================
-// ROTAS DE CLIENTES
+// ROTAS DE CLIENTES (TODAS PROTEGIDAS)
 // ============================================
-router.post('/api/clientes', ClienteController.cadastrar);
-router.put('/api/clientes/:id', ClienteController.atualizar); 
-router.delete('/api/clientes/:id', ClienteController.remover);
-router.get('/api/clientes', ClienteController.todos);
-router.get('/api/clientes/:id', ClienteController.cliente);
+router.post('/api/clientes', Auth.verifyToken, ClienteController.cadastrar);
+router.put('/api/clientes/:id', Auth.verifyToken, ClienteController.atualizar); 
+router.delete('/api/clientes/:id', Auth.verifyToken, ClienteController.remover);
+router.get('/api/clientes', Auth.verifyToken, ClienteController.todos);
+router.get('/api/clientes/:id', Auth.verifyToken, ClienteController.cliente);
+
 
 // ============================================
-// ROTAS DE EMPRESTIMOS
+// ROTAS DE EMPRESTIMOS (TODAS PROTEGIDAS)
 // ============================================
-import EmprestimoController from './controller/EmprestimoController.js';
-router.post('/api/emprestimos', EmprestimoController.cadastrar);
-router.put('/api/emprestimos/:id', EmprestimoController.atualizar);
-router.delete('/api/emprestimos/:id', EmprestimoController.remover);
-router.get('/api/emprestimos', EmprestimoController.todos);
-router.get('/api/emprestimos/:id', EmprestimoController.emprestimo);
+router.post('/api/emprestimos', Auth.verifyToken, EmprestimoController.cadastrar);
+router.put('/api/emprestimos/:id', Auth.verifyToken, EmprestimoController.atualizar);
+router.delete('/api/emprestimos/:id', Auth.verifyToken, EmprestimoController.remover);
+router.get('/api/emprestimos', Auth.verifyToken, EmprestimoController.todos);
+router.get('/api/emprestimos/:id', Auth.verifyToken, EmprestimoController.emprestimo);
 
 export { router };

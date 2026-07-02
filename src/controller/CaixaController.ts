cbdac2e07362ce2f6fb15d2095dccff7a5256f8a
Pedro Roque <roquefiesp@gmail.com>
2026-07-02 16:50:39 -0300

    feat(caixa): cria controller de resumo financeiro

---
+++ src/controller/CaixaController.ts
@@ -0,0 +1,19 @@
+import { type Request, type Response } from "express";
+
+import Caixa from "../model/Caixa.js";
+
+export default class CaixaController {
+    static async resumo(req: Request, res: Response) {
+        try {
+            const resumoFinanceiro = await Caixa.obterResumoFinanceiro();
+
+            res.status(200).json(resumoFinanceiro);
+        } catch (error) {
+            console.error("[CaixaController] Erro ao obter resumo financeiro:", error);
+
+            res.status(500).json({
+                mensagem: "Erro interno ao recuperar o resumo financeiro."
+            });
+        }
+    }
+}

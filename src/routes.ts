e0a3952afd28fe561ba57553b14a733ffd42eadd
Pedro Roque <roquefiesp@gmail.com>
2026-07-02 17:10:39 -0300

    chore: Adiciona rota inicial do Caixa

--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,8 @@ import type { Request, Response } from "express";
 const router = Router();
 
 import ClienteController from "./controller/ClienteController.js";
+import EmprestimoController from './controller/EmprestimoController.js';
+import CaixaController from "./controller/CaixaController.js"; 
 
 // ============================================
 // ROTA INICIAL
@@ -11,7 +13,6 @@ import ClienteController from "./controller/ClienteController.js";
 router.get("/api", (req: Request, res: Response) => {
   res.status(200).json({ mensagem: "Olá, boas-vindas a API do OpenLine." });
 });
-
 
 // ============================================
 // ROTAS DE CLIENTES
@@ -25,11 +26,15 @@ router.get('/api/clientes/:id', ClienteController.cliente);
 // ============================================
 // ROTAS DE EMPRESTIMOS
 // ============================================
-import EmprestimoController from './controller/EmprestimoController.js';
 router.post('/api/emprestimos', EmprestimoController.cadastrar);
 router.put('/api/emprestimos/:id', EmprestimoController.atualizar);
 router.delete('/api/emprestimos/:id', EmprestimoController.remover);
 router.get('/api/emprestimos', EmprestimoController.todos);
 router.get('/api/emprestimos/:id', EmprestimoController.emprestimo);
+
+// ============================================
+// ROTAS DO CAIXA (BALANÇO FINANCEIRO)
+// ============================================
+router.get('/api/caixa', CaixaController.resumo); 
 
 export { router };
